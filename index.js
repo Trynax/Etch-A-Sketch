1a7f66669a11473e670f194c727da65732098512
Trynax <kandoso111@gmail.com>
2023-03-23 13:44:45 +0100

    things getting more complicated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let height = parseInt(prompt("what's your heigth?")) 
+let height = 55;
 let Agrid = document.createElement('div')
 let gridSize = height
 document.body.appendChild(Agrid)
@@ -12,6 +12,7 @@ for (let i=0; i<100; i++){
   grid.style.cssText= `height:${gridSize}px; width:${gridSize}px;border: 0.2px solid gray;`
 Agrid.appendChild(grid)
 console.log(grid.style.height);
+
 }
 
 let grids = document.querySelectorAll('.grid');
@@ -21,4 +22,29 @@ for(let grid of grids){
   grid.addEventListener('mouseenter', function () {
     this.style.backgroundColor ='black';
   })
+  
+}
+
+
+function reset() {
+  let grids = document.querySelectorAll('.grid')
+  for(let i=0; i<grids.length; i++){
+    grids[i].style.backgroundColor='white'
+  }
+}
+function selectGridSize(){
+  gridSize = parseInt(prompt("what's your height?", 66)); 
+  console.log(gridSize);
+  let grids = document.querySelectorAll('.grid');
+  for(let i=0; i<grids.length; i++){
+    grids[i].style.height = `${gridSize}px`;
+    grids[i].style.width = `${gridSize}px`;
+    grids[i].style.backgroundColor = `white`;
+  }
+  let containerWidth = Agrid.offsetWidth;
+  let containerHeight = Agrid.offsetHeight
+  let numRows = Math.floor(containerHeight/gridSize);
+  let numCols = Math.floor(containerWidth/gridSize);
+  Agrid.style.gridTemplateRows =`repeat(${numRows},${gridSize}px)`;
+  Agrid.style.gridTemplateColumns =`repeat(${numCols},${gridSize}px)`;
 }
