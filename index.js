0167856950b0e208ffaeab1431a4e6d58562dfd2
Trynax <kandoso111@gmail.com>
2023-03-27 12:55:43 +0100

    Get the first grid step

--- index.js
+++ index.js
@@ -1,54 +1,40 @@
-let color =  document.getElementById('color');
-let colorChoice = color.value
-let height = 55;
-let Agrid = document.createElement('div')
-document.body.appendChild(Agrid)
-Agrid.classList.add('Agrid')
-let gridSize = height
-console.log(gridSize);
-Agrid.style.cssText= 'display: grid; border: 2px solid black; heigth:500px;width:500px;'
-
-let containerWidth = Agrid.offsetWidth;
-let containerHeight = Agrid.offsetHeight
-let totalAreaOfAgrid = containerHeight*containerWidth
-let totalGridSize = gridSize*gridSize;
-let amountCreadted = Math.floor(totalAreaOfAgrid/totalGridSize);
-for (let i=0; i<100; i++){
-  let grid = document.createElement('div')
-  grid.classList.add('grid')
-  grid.style.cssText= `height:${gridSize}px; width:${gridSize}px;border: 0.2px solid gray;`
-Agrid.appendChild(grid)
-console.log(grid.style.height);
+let gridsContainer = document.createElement('div')
+document.body.appendChild(gridsContainer)
+let gridSize = 50;
+let colorChoice = '#000000'
+let TgridSize = gridSize*gridSize;
+gridsContainer.classList.add('container')
+gridsContainer.style.cssText=`grid-template-columns: repeat(${gridSize},1fr);grid-template-rows: repeat(${gridSize},1fr);`
+for(let i = 0; i<TgridSize; i++){
+   let grid = document.createElement("div")
+   gridsContainer.appendChild(grid)
+   grid.classList.add('grid')
 
 }
 
-let grids = document.querySelectorAll('.grid');
-let lastGrid;
-
-for(let grid of grids){
-  grid.addEventListener('mouseenter', function () {
-    this.style.backgroundColor = colorChoice;
-  })
-  
+let grids = document.querySelectorAll('.grid')
+for (let grid of grids){
+   grid.addEventListener('mouseenter', function (){
+      this.style.backgroundColor=`${colorChoice}`
+   })
 }
-
 
 function reset() {
-  let grids = document.querySelectorAll('.grid')
-  for(let i=0; i<grids.length; i++){
-    grids[i].style.backgroundColor='white'
-  }
+   for(let i=0; i<TgridSize; i++){
+      grids[i].style.backgroundColor='white'
+   }
 }
-function selectGridSize(){
-  gridSize = parseInt(prompt("what's your height?", 66)); 
-  console.log(gridSize);
-  let grids = document.querySelectorAll('.grid');
+
+ function selectGridSize(){
+   gridSize = parseInt(prompt("Select grid size,less than 100", 50))
+   let grids = document.querySelectorAll('.grid');
   for(let i=0; i<grids.length; i++){
-    grids[i].style.height = `${gridSize}px`;
-    grids[i].style.width = `${gridSize}px`;
     grids[i].style.backgroundColor = `white`;
   }
-  
-  Agrid.style.gridTemplateRows =`repeat(${numRows},${gridSize}px)`;
-  Agrid.style.gridTemplateColumns =`repeat(${numCols},${gridSize}px)`;
-}
+  gridsContainer.style.cssText=`margin:0;grid-template-columns: repeat(${gridSize}},1fr);grid-template-rows: repeat(${gridSize},1fr);`
+ }
+
+ let colorInput = document.getElementById('change')
+ colorInput.addEventListener('input', function(){
+   colorChoice=colorInput.value
+ })
